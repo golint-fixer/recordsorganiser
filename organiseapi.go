d4914be197c1473746b050fe19f74a72dfa3e35f
Simon Tucker <brotherlogic@gmail.com>
2016-08-07 10:04:57 -0700

    Enabled the specification of locations. This closes #3.

--- organiseapi.go
+++ organiseapi.go
@@ -2,18 +2,42 @@ package main
 
 import (
 	"flag"
+	"log"
+	"sort"
+	"strconv"
+
+	"golang.org/x/net/context"
 
 	"google.golang.org/grpc"
 
 	"github.com/brotherlogic/goserver"
 
+	pbs "github.com/brotherlogic/discogssyncer/server"
+	pbdi "github.com/brotherlogic/discovery/proto"
+	pbd "github.com/brotherlogic/godiscogs"
 	pb "github.com/brotherlogic/recordsorganiser/proto"
 )
 
-// Server the configuration for the syncer
-type Server struct {
-	*goserver.GoServer
-	saveLocation string
+// Bridge that accesses discogs syncer server
+type prodBridge struct{}
+
+func (discogsBridge prodBridge) getReleases(folders []int32) []*pbd.Release {
+	var result []*pbd.Release
+
+	list := &pbs.FolderList{}
+	for _, id := range folders {
+		list.Folders = append(list.Folders, &pbd.Folder{Id: id})
+	}
+
+	ip, port := getIP("discogsyncer", "10.0.1.17", 50055)
+	conn, _ := grpc.Dial(ip+":"+strconv.Itoa(port), grpc.WithInsecure())
+	defer conn.Close()
+	client := pbs.NewDiscogsServiceClient(conn)
+
+	rel, _ := client.GetReleasesInFolder(context.Background(), list)
+	result = rel.GetReleases()
+
+	return result
 }
 
 // DoRegister does RPC registration
@@ -23,9 +47,48 @@ func (s Server) DoRegister(server *grpc.Server) {
 
 // InitServer builds an initial server
 func InitServer(folder *string) Server {
-	server := Server{&goserver.GoServer{}, *folder}
+	server := Server{&goserver.GoServer{}, *folder, prodBridge{}}
 	server.Register = server
 	return server
+}
+
+func getIP(servername string, ip string, port int) (string, int) {
+	conn, _ := grpc.Dial(ip+":"+strconv.Itoa(port), grpc.WithInsecure())
+	defer conn.Close()
+
+	registry := pbdi.NewDiscoveryServiceClient(conn)
+	entry := pbdi.RegistryEntry{Name: servername}
+	r, _ := registry.Discover(context.Background(), &entry)
+	return r.Ip, int(r.Port)
+}
+
+func test() {
+	var host = flag.String("host", "10.0.1.17", "Hostname of server.")
+	var port = flag.String("port", "50055", "Port number of server")
+	flag.Parse()
+	portVal, _ := strconv.Atoi(*port)
+	dServer, dPort := getIP("discogssyncer", *host, portVal)
+
+	dConn, _ := grpc.Dial(dServer+":"+strconv.Itoa(dPort), grpc.WithInsecure())
+	defer dConn.Close()
+	dClient := pbs.NewDiscogsServiceClient(dConn)
+
+	list := &pbs.FolderList{}
+	list.Folders = append(list.Folders, &pbd.Folder{Name: "12s"})
+
+	releases, err := dClient.GetReleasesInFolder(context.Background(), list)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	sort.Sort(pbd.ByLabelCat(releases.Releases))
+	splits := pbd.Split(releases.Releases, 8)
+	count := 1
+	for _, split := range splits[0] {
+		log.Printf("%v - %v", count, split.Title)
+		count++
+	}
 }
 
 func main() {
