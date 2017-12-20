669c0011e787551a7776287dd79104d2bf8d5c3e
Simon Tucker <brotherlogic@gmail.com>
2017-12-19 22:46:46 -0800

    Adds the option to do a reorg on a pull.

--- organiseapi_test.go
+++ organiseapi_test.go
@@ -30,3 +30,25 @@ func TestGetLocation(t *testing.T) {
 		t.Errorf("Bad location response: %v", resp)
 	}
 }
+func TestGetLocationOrgFail(t *testing.T) {
+	testServer := getTestServer(".testAddLocation")
+	location := &pb.Location{
+		Name:      "TestName",
+		Slots:     2,
+		FolderIds: []int32{10},
+		Sort:      pb.Location_BY_DATE_ADDED,
+	}
+
+	_, err := testServer.AddLocation(context.Background(), &pb.AddLocationRequest{Add: location})
+	if err != nil {
+		t.Fatalf("Unable to add location: %v", err)
+	}
+
+	testServer.bridge = testBridgeFail{}
+
+	_, err = testServer.GetOrganisation(context.Background(), &pb.GetOrganisationRequest{ForceReorg: true, Locations: []*pb.Location{&pb.Location{Name: "TestName"}}})
+	if err == nil {
+		t.Fatalf("Failing bridge did not fail reorg %v", err)
+	}
+
+}
